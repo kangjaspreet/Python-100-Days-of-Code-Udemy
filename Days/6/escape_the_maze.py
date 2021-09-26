e8069eb12cf8acc4458d0ba958cd71f4ac253109
kangjaspreet <jaspreetkang@g.ucla.edu>
2021-09-26 16:38:23 -0700

    Add Day 6 Project - Escaping the maze

---
+++ Days/6/escape_the_maze.py
@@ -0,0 +1,18 @@
+# Escape the maze - This script won't run locally. Need to go to this site:
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+        
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    elif wall_in_front():
+        turn_left()
